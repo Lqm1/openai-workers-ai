695d6e101c2cd4abd21c5c80a1c729470ba5c709
Lami <info@lami.zip>
2025-02-23 18:51:11 +0900

    feat(index.ts): add logger middleware to improve debugging

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { Hono } from "hono";
+import { logger } from "hono/logger";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
+
+app.use(logger());
 
 app.get("/", (c) => {
 	return c.text("Hello Hono!");
